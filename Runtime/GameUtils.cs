067be75791a0afaf09e5a7809ddc758403b0e331
Denis Angell <denisangell@Deniss-MBP.lan>
2022-08-03 00:04:27 -0400

    initial

---
+++ Runtime/GameUtils.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xrpl.C.Unity {
+    public class GameUtils : MonoBehaviour
+    {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    }
+}
